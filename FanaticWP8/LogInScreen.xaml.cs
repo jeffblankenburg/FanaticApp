cfa1e583af572fd99e281c7c62dddba42b9d8759
Jeff Blankenburg <github@jeffblankenburg.com>
2014-02-13 15:02:52 -0500

    Added Facebook Nuget.  Got login working.
    For better or worse, it works.  Not sure how I make additional calls,
    but will need to look into it more.

--- FanaticWP8/LogInScreen.xaml.cs
+++ FanaticWP8/LogInScreen.xaml.cs
@@ -28,7 +28,12 @@ namespace FanaticWP8
         private async void CheckAuthentication(Image i)
         {
             if (i.Name.Contains("Microsoft")) await Authenticate(MobileServiceAuthenticationProvider.MicrosoftAccount);
-            else if (i.Name.Contains("Facebook")) await Authenticate(MobileServiceAuthenticationProvider.Facebook);
+            else if (i.Name.Contains("Facebook"))
+            {
+                string path = "fbconnect://authorize?client_id={137042686448025}&redirect_uri=msft-{f1f9b070-f351-499a-8a12-2ccee6095f76}:";
+                Uri uri = new Uri(path);
+                var success = await Windows.System.Launcher.LaunchUriAsync(uri);
+            }//await Authenticate(MobileServiceAuthenticationProvider.Facebook);
             else if (i.Name.Contains("Twitter")) await Authenticate(MobileServiceAuthenticationProvider.Twitter);
             else if (i.Name.Contains("Google")) await Authenticate(MobileServiceAuthenticationProvider.Google);
         }
@@ -53,6 +58,12 @@ namespace FanaticWP8
 
                 MessageBox.Show(message);
             }
+        }
+
+        private void FacebookSessionStateChanged(object sender, Facebook.Client.Controls.SessionStateChangedEventArgs e)
+        {
+            this.ContentPanel.Visibility = (e.SessionState == Facebook.Client.Controls.FacebookSessionState.Opened) ?
+                                Visibility.Visible : Visibility.Collapsed;
         }
     }
 }
